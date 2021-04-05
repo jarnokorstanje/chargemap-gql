e39204c86bdcb496b7b055d754c3337b32f15652
jarnokorstanje <kors0023@hz.nl>
2021-04-05 13:51:49 +0300

    get all stations

---
+++ resolvers/stationResolver.js
@@ -0,0 +1,14 @@
+import Station from '../models/station.js';
+
+export default {
+    Query: {
+        stations: async (parent, args) => {
+            try {
+                const res = await Station.find();
+                return res;
+            } catch (e) {
+                console.log(`Error occured while getting the stations: ${e.message}`);
+            }
+        },
+    },
+};
